4491cdb6f294c50de2cf72ed93622222c9b3af11
Tanya Sergienko <chocolate1378@gmail.com>
2019-02-08 09:35:47 +0200

    particles

--- genetic_algorithms/particles.py
+++ genetic_algorithms/particles.py
@@ -1,53 +1,78 @@
-import random
+# particle swarm optimization
+
+from random import random, randint, seed, Random
 import numpy
+from copy import copy
 import math
 
-#particle swarm oprimization
+particles_amount = 20
+dim = 2
+w = 0.729
+c1 = 1.49445
+c2 = 1.49445
+b_lo = -5.12
+b_up = 5.12
+iterations = 200
 
-a = -10
-b = 10
-particles_amount = 50
-dim = 3
-w = 2
-b_lo = -5
-b_up = 5
-A = 10
-# criteria: iteration amount or best not changing
 def criteria(particles):
     return True
 
-def f(X, A):
-    return A + sum([x ** 2 + A * math.cos(2 * math.pi * x) for x in X])
+def f(X):
+    # rastrigin function
+    n = len(X)
+    r = 0
+    for i in range(n):
+        r += X[i] * X[i] - 10 * math.cos(2 * math.pi * X[i]) + 10
+    return r
+
+class Particle():
+    def __init__(self, my_seed):
+        self.rndm = Random(my_seed)
+        self.position = [0.0 for i in range(dim)]
+        self.velocity = [0.0 for i in range(dim)]
+
+        for k in range(dim):
+            self.position[k] = (b_up - b_lo) * self.rndm.random() + b_lo
+            self.velocity[k] = (b_up - b_lo) * self.rndm.random() + b_lo
+        self.my_best_pos = copy(self.position)
+
+
 
 def particle_optimization():
-    zeros_array = numpy.zeros( (particles_amount, dim) )
-    particles = zeros_array
-    next_pos = numpy.zeros((dim, 1))
-    best_pos = zeros_array
-    velocity = zeros_array
-    gbest = zeros_array
-    # init starting position
+    rndm = Random(0)
+    swarm = [Particle(i) for i in range(particles_amount)]  
+    gbest = copy(swarm[0].position)
     for i in range(particles_amount):
-        for k in range(dim):
-            particles[i][k] = random.randint(b_lo, b_up)
-        best_pos[i] = particles[i]
-        if f(particles[i], A) > f(gbest[i], A):
-            gbest[i] = particles[i]
-        velocity[i] = (random.random(), random.random(), random.random())
-    print(particles)
-    while not criteria(particles):
+        if f(swarm[i].position) < f(gbest):
+            gbest = copy(swarm[i].position)
+
+    for epoch in range(0, iterations):
+        if epoch % 20 == 0:
+            print("Fitness function value: " + str(f(gbest)))
         for i in range(particles_amount):
             for j in range(dim):
-                rp = random.random()# rp and rg from[0;1]
-                rg = random.random()
-                velocity[i][j] = w * velocity[i][j] + rp * (best_pos[i][j] - particles[i][j]) * random.random() + \
-                    rg * (gbest[i][j] - particles[i][j]) * random.random()
-            next_pos[i] = particles[i] + velocity[i]
-            if f(next_pos[i][j], A) < f(particles[i], A):
-                particles[i] = next_pos[i]
-                if f(particles[i], A) < f(gbest[i], A):
-                    gbest[i] = particles[i] 
+                rp = rndm.random()
+                rg = rndm.random()
+                swarm[i].velocity[j] = w * swarm[i].velocity[j] + \
+                    c1* rp * (swarm[i].my_best_pos[j] - swarm[i].position[j]) + \
+                    c2 * rg * (gbest[j] - swarm[i].position[j])
 
+            for d in range(dim):
+                swarm[i].position[d] += swarm[i].velocity[d]
+                if swarm[i].position[d] < b_lo:
+                    swarm[i].position[d] = b_lo
+                elif swarm[i].position[d] > b_up:
+                    swarm[i].position[d] = b_up
+            if f(swarm[i].position) < f(swarm[i].my_best_pos):
+                print("better----> " + str(swarm[i].position))
+                swarm[i].my_best_pos = copy(swarm[i].position)
+                if f(swarm[i].my_best_pos) < f(gbest):
+                    gbest = copy(swarm[i].my_best_pos)
+                    print("New global best pos: " + str(gbest))
+    return gbest
 
 if __name__ == "__main__":
-    particle_optimization()
+    print("Particle swarm optimization")
+    res = particle_optimization()
+    print("Result: " + str(res))
+    print("Fitness in " + str(res) + " = " + str(f(res)) )
