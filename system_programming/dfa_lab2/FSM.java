89a8d2c3a8c541bdca2ceb0d5154271df9221114
Tanya Sergienko <chocolate1378@gmail.com>
2019-03-31 22:24:29 +0300

    fsm

--- system_programming/dfa_lab2/FSM.java
+++ system_programming/dfa_lab2/FSM.java
@@ -5,11 +5,14 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Queue;
+import java.util.LinkedList;
+
 
 public class FSM {
     Set<Character> alphabet;
     Set<Integer> states;
-    Character initial;
+    Integer initial;
     Set<Integer> finals;
     Map<Integer, Map<Character, Set<Integer>>> transitions;
 
@@ -28,31 +31,105 @@ public class FSM {
         {
             Scanner sc = new Scanner(file);
             String[] alphs = sc.nextLine().split("\\s");
+            alphabet = new HashSet<>();
             for (int i = 0; i < alphs.length; i++){
                 alphabet.add(Character.valueOf(alphs[i].charAt(0)));
             }
 
-            String[] states_line = sc.nextLine().split("\\s");
-            for (int i = 0; i < alphs.length; i++){
-                states.add(Integer.valueOf(alphs[i]));
+            int numberOfStates = sc.nextInt();
+            states = new HashSet<>(numberOfStates);
+            for (int i = 0; i < numberOfStates; i++){
+                states.add(i);
             }
 
-            String in = sc.nextLine();
-            initial = Character.valueOf(in.charAt(0));
+            initial = sc.nextInt();
+
+
             int amount_fin_states = sc.nextInt();
+            finals = new HashSet<>(amount_fin_states);
             for (int i = 0; i < amount_fin_states; i++) {
                 finals.add(sc.nextInt());
             }
 
+            transitions = new HashMap<>(numberOfStates);
+            for (Integer state : states) {
+                transitions.put(state, new HashMap<>());
+            }
             int j = 0;
             while (sc.hasNext()) {
-                transitions
+                Integer fromState = sc.nextInt();
+                Character viaLetter = sc.next().charAt(0);
+                Integer toState = sc.nextInt();
+                if (!transitions.get(fromState).keySet().contains(viaLetter)){
+                    transitions.get(fromState).put(viaLetter, new HashSet<>());
+                }
+                transitions.get(fromState).get(viaLetter).add(toState);
             }
-
-            
         }
         catch (FileNotFoundException ex) {
-            System.out.println("Invalid file pathname");
+            System.out.println("Invalid file");
         }
     }
+    
+    private Map<Integer, Boolean> bfsFromStates(Set<Integer> fromStates) {
+        Map<Integer, Boolean> used = new HashMap<>();
+        for (Integer state : states) {
+            used.put(state, false);
+        }
+
+        Queue<Integer> statesq = new LinkedList<>(fromStates);
+        while (!statesq.isEmpty()) {
+            Integer fromState_ = statesq.peek();
+            used.put(fromState_, true);
+
+            for (Character viaLetter : transitions.get(fromState_).keySet()) {
+                for (Integer toState : transitions.get(fromState_).get(viaLetter)) {
+                    if (!used.get(toState)) {
+                        statesq.add(toState);
+                    }
+                }
+            }
+
+            statesq.poll();
+        }
+        return used;
+    }
+    
+    Set<Integer> getReachableFromStates(Set<Integer> fromStates) {
+        Map<Integer, Boolean> used = bfsFromStates(fromStates);
+        Set<Integer> visited = new HashSet<>();
+        for (Integer state : used.keySet()) {
+            if (used.get(state)) {
+                visited.add(state);
+            }
+        }
+        return visited;
+    }
+
+    private Set<Integer> getReachableFromState(Integer fromState) {
+        Set<Integer> fromStates = new HashSet<>();
+        fromStates.add(fromState);
+        return getReachableFromStates(fromStates);
+    }
+
+    Set<Integer> getReachable() {
+        return getReachableFromState(initial);
+    }
+
+    Set<Integer> processWordFromStates(String word, Set<Integer> fromStates) {
+        Set<Integer> states_ = new HashSet<>(fromStates);
+        for (Character viaLetter : word.toCharArray()) {
+            Set<Integer> nextStates = new HashSet<>();
+            for (Integer fromState_ : states_) {
+                System.out.println("from: " + fromState_);
+                System.out.println(transitions.get(fromState_).get(viaLetter));
+                if (transitions.get(fromState_).containsKey(viaLetter)){
+                    nextStates.addAll(transitions.get(fromState_).get(viaLetter));
+                }
+            }
+            states_ = new HashSet<>(nextStates);
+        }
+        return states_;
+    }
+
 }
--- system_programming/dfa_lab2/Main.java
+++ system_programming/dfa_lab2/Main.java
@@ -1,21 +1,46 @@
 import java.io.File;
 import java.util.Scanner;
-
 import javax.lang.model.util.ElementScanner6;
-
 import java.io.FileNotFoundException;
+import java.util.Set;
 
 public class Main{
 
     public static void main(String[] args)
     {
-        if (args.length != 0)
-            RtFiniteStateMachine fsm = FSMBuilder::build(args[0]);
-        else
-            System.out.println("Please, enter filename with finite state machine");
-        
+        Scanner sc = new Scanner(System.in);
+        // System.out.format("Please enter the name of file with NFA: ");
+        // String pathname = sc.next();
 
+        String pathname = "automat1";
+        File file = new File(pathname);
+        FSM fsm = new FSM(file);
+
+        Set<Integer> reachableStates = fsm.getReachable();
+        System.out.println("reachable");
+        for (Integer st: reachableStates){
+            System.out.println(st);
+        }
         
+        System.out.format("Enter w0: ");
+        String w0 = sc.next();
+        Set<Integer> afterW0 = fsm.processWordFromStates(w0, reachableStates);
+        System.out.println("After w0");
+        for (Integer st: afterW0){
+            System.out.println(st);
+        }
+        Set<Integer> reachableFromAfterW0 = fsm.getReachableFromStates(afterW0);
+        System.out.println("reachable after fromw0");
+        for (Integer st: reachableFromAfterW0){
+            System.out.println(st);
+        }
+        reachableFromAfterW0.retainAll(fsm.finals);
+        if (reachableFromAfterW0.isEmpty()) {
+            System.out.println("There does NOT exist w1, w2 such that w1w0w2 is acceptable.");
+        } else {
+            System.out.println("There DOES exist w1, w2 such that w1w0w2 is acceptable.");
+        }
+        sc.close();      
 
         
     }
