2017106cdbb0ebf119d9dd1b2c2812abcefa5e90
Tanya Sergienko <chocolate1378@gmail.com>
2019-04-20 20:55:49 +0300

    4 lab

--- eko_processes/eko4.py
+++ eko_processes/eko4.py
@@ -28,28 +28,69 @@ print("A = ", A)
 def production(cl):
     return A * cl[0]**a * cl[1]**(1-a)
 
-def costs(cl):
+def cost(cl):
     return w1 * cl[0] + w2 * cl[1]
 
 def profit(cl):
-    return p * production(cl) - costs(cl)
+    return p * production(cl) - cost(cl)
 
 def minus_profit(cl):
     return - profit(cl)
 
 
-cl_star_1 = minimize(
+cl_star_max_profit = minimize(
     minus_profit,
     x0=(10_000, 10_000),
     constraints=(
-    {'type': 'ineq', 'fun': lambda cl: costs(cl)},
-    {'type': 'ineq', 'fun': lambda cl: TC - costs(cl)},
+    {'type': 'ineq', 'fun': lambda cl: cost(cl)},
+    {'type': 'ineq', 'fun': lambda cl: TC - cost(cl)},
     )
     )['x']
 print(
-    'limited cost, maximal profit:\n'
-    f' cl_star = {cl_star_1}\n'
-    f' costs(cl_star) = {costs(cl_star_1)}\n'
-    f' product(cl_star) = {production(cl_star_1)}\n'
-    f' profit(cl_star) = {profit(cl_star_1)}\n'
+    'Limited cost, maximal profit:\n'
+    f' cl_star = {cl_star_max_profit}\n'
+    f' costs(cl_star) = {cost(cl_star_max_profit)}\n'
+    f' product(cl_star) = {production(cl_star_max_profit)}\n'
+    f' profit(cl_star) = {profit(cl_star_max_profit)}\n'
     )
+
+
+#######################
+
+cl_star_2 = minimize(
+    minus_profit,
+    x0=(10_000, 10_000),
+    constraints=(
+    {'type': 'ineq', 'fun': lambda cl: cost(cl)},
+    {'type': 'ineq', 'fun': lambda cl: TC - cost(cl)},
+    )
+    )['x']
+
+print(
+    'Limited cost, maximal production:\n'
+    f' cl_star = {cl_star_2}\n'
+    f' cost(cl_star) = {cost(cl_star_2)}\n'
+    f' product(cl_star) = {production(cl_star_2)}\n'
+    f' profit(cl_star) = {profit(cl_star_2)}\n'
+)
+
+##########################
+
+cl_star_3 = minimize(
+    cost,
+    x0=(10_000, 10_000),
+    constraints=(
+    {'type': 'eq', 'fun': lambda cl: production(cl) - q0},
+    )
+    )['x']
+
+print(
+    'Fixed production, minimal cost:\n'
+    f' cl_star = {cl_star_3}\n'
+    f' cost(cl_star) = {cost(cl_star_3)}\n'
+    f' product(cl_star) = {np.round(production(cl_star_3))}\n'
+    f' profit(cl_star) = {profit(cl_star_3)}\n'
+)
+
+
+
