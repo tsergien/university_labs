6e01d82306a8426f0552d738a1d6c1056eb31a51
Tanya Sergienko <chocolate1378@gmail.com>
2019-11-11 09:43:10 +0200

    gen

--- genetic_algorithms/geneticDec.py
+++ genetic_algorithms/geneticDec.py
@@ -6,23 +6,54 @@ import random
 from functools import reduce
 from typing import Tuple
 from itertools import accumulate 
+import time
 
+def rastrigin(x: np.array) -> float:
+	return -10 * _m - np.sum(np.power(x, 2) - 10 * np.cos(2 * np.pi * x))
 
-def CodDecimal(xbin: np.array, xmin: float, d: float):
+# -----> ackley function : x in [-5, 5]
+def ackley(x: np.array) -> float:
+    return -20 * math.exp(-0.2 * math.sqrt(x[0]**2 + x[1]**2)) - \
+        math.exp(0.5 * ( math.cos(2*math.pi*x[0]) + math.cos(2*math.pi*x[1]) ) ) + \
+            math.e + 20
+
+# -----> rosenbroke function
+def rosenbroke(x: np.array) -> float:
+    r = 0
+    for i in range(len(x) - 1):
+        r += 100 * (x[i+1] - x[i]*x[i])**2 + (x[i] - 1)**2
+    return r
+
+def _cod_decimal(xbin: np.array, xmin: float, d: float) -> float:
 	xdec1 = reduce(lambda _1, _2: _1 * 2 + _2, map(int, xbin[::-1]))
 	return xmin + d * xdec1
 
-def CodBinary(xdec: float, xmin: float, l: int, d: float) -> np.array:
+def CodDecimal(rows: int, cols: int, g_bin: np.matrix, x_min: np.array, NN: np.array,
+                    dd: np.array) -> np.matrix:
+    _m: int = NN.shape[0] - 1
+    return np.matrix([[
+        _cod_decimal(np.asarray(g_bin[i, NN[j]:NN[j + 1]]).reshape(-1), x_min[j], dd[j])
+    for j in range(_m)] for i in range(n)])
+
+def _cod_binary(xdec: float, xmin: float, l: int, d: float) -> np.array:
 	xx = int(np.floor((xdec - xmin) / d))
 	digits = list(map(int, bin(xx)[:1:-1]))
 	return np.array(digits + [0 for _ in range(l - len(digits))])
+
+def CodBinary(n: int, m: int, g_dec: np.matrix, x_min: np.array,
+                   nn: np.array, dd: np.array) -> np.matrix:
+    return np.matrix([
+        np.hstack([
+            _cod_binary(g_dec[i, j], x_min[j], nn[j], dd[j]) for j in range(m)
+        ]) for i in range(n)
+    ])
 
 def GenerationDec(rows: int, cols: int, x_min: np.array, x_max: np.array) -> np.matrix:
 	return  np.random.uniform(low=x_min, high=x_max, size=(rows, cols))
 
 def BinDecParam(rows: int, cols: int, x_min: np.array, x_max: np.array, 
 					eps: float) -> Tuple[np.array, np.array, np.array]:    
-	nn = np.floor(np.log2((x_max - x_min) / eps)) + 1
+	nn = (np.floor(np.log2((x_max - x_min) / eps)) + 1).astype(int)
 	dd = np.round((x_max - x_min) / (np.power(2, nn) - 1), 7)
 	NN = np.array([0,] + list(accumulate(nn)))
 	return nn, dd, NN
@@ -31,7 +62,8 @@ def Parens(n: int):
 	p = np.random.permutation(np.arange(n << 1))
 	return p[:n], p[n:]
 
-def Adapt(periodic: np.array, n: int) -> np.array:
+def Adapt(periodic: np.array) -> np.array:
+	n : int = len(periodic)
 	periodic -= np.min(periodic)
 	periodic /= np.sum(periodic)
 	num = np.zeros(n)
@@ -40,52 +72,89 @@ def Adapt(periodic: np.array, n: int) -> np.array:
 	return num.astype(int)
 
 def Mutation(G: np.matrix, p: float) -> Tuple[np.matrix, int]:
-	mask = np.random.uniform(size=g.shape) < p
-	return g ^ mask, np.sum(np.sum(mask))
+	mask = np.random.uniform(size=G.shape) < p
+	return G ^ mask, np.sum(np.sum(mask))
 
 def Crossover(G: np.matrix, n: int, m_list: np.array, f_list: np.array):
-	cros = np.random.randint(low=0, high=g.shape[1], size=n)
-	g_cros = np.copy(g)
+	cros = np.random.randint(low=0, high=G.shape[1], size=n)
+	g_cros = np.copy(G)
 	for m, f, c in zip(m_list, f_list, cros):
 		g_cros[[m, f], :c] = g_cros[[f, m], :c]
 	return g_cros
 
 
-if __name__ == "__main__":
-	# mlist, flist = Parens(N)
-	g_dec = np.matrix([[1.1, 2.31], [1.3, 2.7]])
-	x_min = np.array([1, 2])
-	nn = np.array([3, 4])
-	dd = np.array([.1, .1])
-	
-	codbin = CodBinary(1.25, 1, 10, 0.1)
-	print(f'codbin = CodBinary(1.25, 1, 10, 0.1):', codbin)
-	print(f'CodDecimal(codbin, 1, 0.05):', CodDecimal(codbin, 1, 0.05))
+def best(m: int, g_dec: np.matrix) -> int:
+	return np.argmax(g_dec[:, m])
 
-	print(f'GenerationDec(3, 2, np.array([1, 1]), np.array([2, 2])): ', GenerationDec(3, 2, np.array([1, 1]), np.array([2, 2])) )
-	nn, dd, NN = BinDecParam(3, 2, np.array([1, 1]), np.array([2, 2]), 0.01)
-	print(f'BinDecParam nn: ', nn)
-	print(f'BinDecParam dd: ', dd)
-	print(f'BinDecParam NN: ', NN)
+def worst(m: int, g_dec: np.matrix) -> int:
+	return np.argmin(g_dec[:, m])
 
-	periodic = np.array([1.1, 1.3, 1.7, 1.9])
-	print("Adapt: ", Adapt(periodic, len(periodic)))
 
-	print("Parens(10): ", Parens(10))
-	
-	# mutation 
-	g = np.random.randint(low=0, high=2, size=(10, 20))
-	g_mut, s_mut = Mutation(g, 1e-1)
-	print(g_mut, s_mut, sep='\n')
+def new_generation(g_dec: np.matrix, num: np.array) -> np.matrix:
+	return np.repeat(g_dec, repeats=num, axis=0)
 
-	#crossover
-	g = np.matrix([
-		[1, 1, 1, 1, 1, 1], 
-		[1, 1, 1, 1, 1, 1], 
-		[0, 0, 0, 0, 0, 0], 
-		[0, 0, 0, 0, 0, 0],
-	])
-	m_list, f_list = np.array([0, 1]), np.array([2, 3])
-	print(Crossover(g, len(g)//2, m_list, f_list))
-	
+#########################################################################
+#########################################################################
+#########################################################################
+#########################################################################
+
+
+np.set_printoptions(linewidth=100)
+np.random.seed(65537)
+n, _m = 1 << 10, 2
+print(f'Population: {n}, dimension: {_m}')
+p = 2e-2
+
+x_min, x_max = np.repeat(-5.12, _m), np.repeat(5.12, _m)
+# x_min, x_max = np.repeat(-5, _m), np.repeat(5, _m)
+g_dec = GenerationDec(n, _m, x_min, x_max)
+eps = 1e-5
+nn, dd, NN = BinDecParam(n, _m, x_min, x_max, eps)
+
+# print(
+# 	f'nn = {nn}\n'
+# 	f'dd = {dd}\n'
+# 	f'NN = {NN}'
+# )
+
+t0 = time.time()
+for _ in range(1, 1 << 12): # 12
+	g_bin = CodBinary(n, _m, g_dec, x_min, nn, dd)
+
+	g_bin, mutation_count = Mutation(g_bin, p)
+
+	m, f = Parens(n >> 1)
+
+	g_bin = Crossover(g_bin, n, m, f)
+
+	g_dec = CodDecimal(n, _m, g_bin, x_min, NN, dd)
+
+	f_vals = np.array([rastrigin(g_dec[i]) for i in range(n)]).reshape((n, 1))
+	# f_vals = np.array([rosenbroke(g_dec[i]) for i in range(n)]).reshape((n, 1))
+	# f_vals = np.array([ackley(g_dec[i]) for i in range(n)]).reshape((n, 1))
+
+	if not (_ & 127):
+		print(f'Iteration: {_}, max fitness function value: {np.max(f_vals)}')
+
+	g_dec = np.hstack([g_dec, f_vals])
+
+	b, w = best(_m, g_dec), worst(_m, g_dec)
+
+	g_best = np.asarray(g_dec[b, :-1]).flatten()
+
+	g_dec = np.delete(g_dec, w, axis=0)
+
+
+	num = Adapt(np.asarray(g_dec[:, -1]).flatten())
+
+	g_dec = np.vstack([new_generation(g_dec[:, :-1], num), g_best])
+
+
+print(f'Generation: {g_dec}')
+print(f'Time: {np.round(time.time() - t0)} seconds')
+
+f_vals = np.array([rastrigin(g_dec[i]) for i in range(n)]).reshape((n, 1))
+
+print(f'Best individual: {np.min(np.abs(f_vals))}')
+
 
--- genetic_algorithms/particles.py
+++ genetic_algorithms/particles.py
@@ -1,13 +1,15 @@
-# particle swarm optimization
+#!/usr/bin/python3
 
 from random import random, randint, seed, Random
 import numpy
 from copy import copy
 import math
 import time
+import matplotlib.pyplot as plt
+
 
 particles_amount = 50
-dim = 4
+dim = 2
 w = 0.729
 c1 = 1.49445
 c2 = 1.49445
@@ -16,20 +18,16 @@ b_up = 5.12
 iterations = 500
 
 # -----> rastrigin function
-def f(X):
-    n = len(X)
-    res = 0
-    for i in range(n):
-        res += X[i] * X[i] - 10 * math.cos(2 * math.pi * X[i]) + 10
-    return res
-
-# -----> sphere function
 # def f(X):
 #     n = len(X)
-#     r = 0
+#     res = 0
 #     for i in range(n):
-#         r += X[i] * X[i]
-#     return r
+#         res += X[i] * X[i] - 10 * math.cos(2 * math.pi * X[i]) + 10
+#     return res
+
+# -----> sphere function
+def f(X):
+    return numpy.sum([i*i for i in X])
 
 # -----> rosenbroke function
 # def f(X):
@@ -48,18 +46,19 @@ def f(X):
 
 class Particle():
     def __init__(self, my_seed):
-        self.rndm = Random(my_seed)
+        self.random = Random(my_seed)
         self.position = [0.0 for i in range(dim)]
         self.velocity = [0.0 for i in range(dim)]
 
         for k in range(dim):
-            self.position[k] = (b_up - b_lo) * self.rndm.random() + b_lo
-            self.velocity[k] = (b_up - b_lo) * self.rndm.random() + b_lo
+            self.position[k] = (b_up - b_lo) * self.random.random() + b_lo
+            self.velocity[k] = (b_up - b_lo) * self.random.random() + b_lo
         self.my_best_pos = copy(self.position)
 
 
+plot_values = {}
 def particle_optimization():
-    rndm = Random(0)
+    random = Random(0)
     swarm = [Particle(i) for i in range(particles_amount)]  
     gbest = copy(swarm[0].position)
     for i in range(particles_amount):
@@ -68,12 +67,10 @@ def particle_optimization():
 
     t1 = time.time()
     for epoch in range(0, iterations):
-        if epoch % 20 == 0:
-            print("Fitness function value: " + str(f(gbest)))
         for i in range(particles_amount):
             for j in range(dim):
-                rp = rndm.random()
-                rg = rndm.random()
+                rp = random.random()
+                rg = random.random()
                 swarm[i].velocity[j] = w * swarm[i].velocity[j] + \
                     c1* rp * (swarm[i].my_best_pos[j] - swarm[i].position[j]) + \
                     c2 * rg * (gbest[j] - swarm[i].position[j])
@@ -85,20 +82,25 @@ def particle_optimization():
                 elif swarm[i].position[d] > b_up:
                     swarm[i].position[d] = b_up
             if f(swarm[i].position) < f(swarm[i].my_best_pos):
-                print("new best position----> " + str(swarm[i].position))
                 swarm[i].my_best_pos = copy(swarm[i].position)
                 if f(swarm[i].my_best_pos) < f(gbest):
                     gbest = copy(swarm[i].my_best_pos)
-                    print("New global best pos: " + str(gbest))
-    print("\033[1;32m Time: \033[0;0m" + str(time.time() - t1))
+                    print(f'New global best pos: {gbest}')
+        plot_values[epoch] = f(gbest)
+    print(f'\033[1;32m Time: \033[0;0m {time.time() - t1}')
+    # print(f'plot vals [0] {plot_values[:][0]}')
+    plt.plot(plot_values.keys(), plot_values.values(), 'k-', label='gbest error')
+    plt.xlabel('iteration')
+    plt.ylabel('error')
+    plt.title('Particle swarm optimization')
+    plt.show()
     return gbest
 
 
+
 n_round = 2
-if __name__ == "__main__":
-    print("Particle swarm optimization")
+if __name__ == '__main__':
+    print('Particle swarm optimization')
     res = particle_optimization()
-    # for i in range(dim):
-    #     res[i] = round(res[i], n_round)
-    print("\033[1;32m Result:  \033[0;0m" + str(res))
-    print("\033[1;32m Fitness in  \033[0;0m" + str(res) + " = " + str(f(res)) )
+    print(f'\033[1;32m Result:  \033[0;0m {res}')
+    print(f'\033[1;32m Fitness in  \033[0;0m" {res} = {f(res)}' )
